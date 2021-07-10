d56a413b38de6796df712d02e85535e280204874
Josue Muñoz Avila <sjosue5082002@gmail.com>
2021-07-10 18:13:40 -0500

    Estilo al formulario

--- index.php
+++ index.php
@@ -19,11 +19,11 @@
             <div class="campo">
                 <label for="telefono">Telefono:</label>
                 <input type="tel" placeholder="Telefono Contacto" id="telefono">
-            </div> 
-            <div class="enviar">
-                <input type="submit" value="AÑADIR">
             </div>      
         </div>
+        <div class="campo enviar">
+                <input type="submit" value="AÑADIR">
+            </div> 
     </form>
 </div>
 
