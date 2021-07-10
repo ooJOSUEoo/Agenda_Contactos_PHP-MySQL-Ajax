c17e6be9fd1a1058ac9e223a12b3b814b59858ef
Josue Muñoz Avila <sjosue5082002@gmail.com>
2021-07-10 13:21:28 -0500

    Archivos iniciales

---
+++ inc/layout/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda de Contactos</title>
+    <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.0/normalize.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/css/agenda.css">
+</head>
+<body>
+    
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include './inc/layout/header.php'; ?>
+
+<div class="contenedor-barra">
+    <h1>Agenda de Contactos</h1>
+</div>
+
+<?php include './inc/layout/footer.php'; ?>
