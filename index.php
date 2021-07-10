b96f17cc41079f26c4d6720427fc5eeeee6dcba6
Josue Muñoz Avila <sjosue5082002@gmail.com>
2021-07-10 16:08:00 -0500

    Creando la lista de contactos

--- index.php
+++ index.php
@@ -26,6 +26,59 @@
         </div>
     </form>
 </div>
-
+<div class="bg-blanco contenedor sombra contactos">
+    <div class="contenedor-contactos">
+        <h2>Contactos</h2>
+        <input type="text" id="buscar" class="buscador sombra" placeholder="Buscar Contactos...">
+        <p class="total-contactos"><span>2 </span>Contactos</p>
+        <div class="contenedor-tabla">
+            <table id="listado-contactos">
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Empresa</th>
+                        <th>Telefono</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Josue</td>
+                        <td>UTT</td>
+                        <td>1234567890</td>
+                        <td>
+                            <a href="#" class="btn-editar btn"><i class="fa fa-pencil-square" aria-hidden="true"></i></a>
+                            <button data-id="1" type="button" class="btn-borrar btn">
+                                    <i class="fa fa-trash" aria-hidden="true"></i>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Josue</td>
+                        <td>UTT</td>
+                        <td>1234567890</td>
+                        <td>
+                            <a href="#" class="btn-editar btn"><i class="fa fa-pencil-square" aria-hidden="true"></i></a>
+                            <button data-id="1" type="button" class="btn-borrar btn">
+                                    <i class="fa fa-trash" aria-hidden="true"></i>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Josue</td>
+                        <td>UTT</td>
+                        <td>1234567890</td>
+                        <td>
+                            <a href="#" class="btn-editar btn"><i class="fa fa-pencil-square" aria-hidden="true"></i></a>
+                            <button data-id="1" type="button" class="btn-borrar btn">
+                                    <i class="fa fa-trash" aria-hidden="true"></i>
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 
 <?php include './inc/layout/footer.php'; ?>
