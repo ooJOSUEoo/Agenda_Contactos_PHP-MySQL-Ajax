ef82884d44c03a525d94bfbeb31c51f90d5826ae
Josue Muñoz Avila <sjosue5082002@gmail.com>
2021-07-12 20:22:02 -0500

    Creando una notificacion cuando el formulario este vacio

--- js/app.js
+++ js/app.js
@@ -16,8 +16,30 @@ function leerFormulario(e) {
          telefono = document.querySelector('#telefono').value;
 
     if (nombre === '' || empresa === '' || telefono === '') {
-
+        //2 parametros, texto y clase
+        mostrarNotificacion('Todos los campos son obligatorios', 'error');
     }else{
         
     }
+}
+//Notificacion en pantalla
+function mostrarNotificacion(mensaje, clase) {
+    const notificacion = document.createElement('div');
+    notificacion.classList.add(clase,'notificacion','sombra')
+    notificacion.textContent = mensaje;
+
+    //Formulario
+    formularioContactos.insertBefore(notificacion, document.querySelector('form legend'));
+
+    //Ocultar y mostrar la notificacion
+    setTimeout(() => {
+        notificacion.classList.add('visible');
+
+        setTimeout(() => {
+            notificacion.classList.remove('visible');
+            setTimeout(() => {
+                notificacion.remove();
+            }, 600);
+        }, 3000);
+    }, 100);
 }
